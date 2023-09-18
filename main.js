ab6aa479ec79ffb720e8a0d292cf9b5cfeabe70b
sahilpatel123 <sahilpatel@wustl.edu>
2023-09-18 07:55:48 -0500

    closer + hamburger fix

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerButton = document.getElementById("hamburger");
     const mobileLinks = document.getElementById("mobileLinks");
-
+    mobileLinks.style.display = "none";
     hamburgerButton.addEventListener("click", function (e) {
         e.stopPropagation(); // Prevent click event from reaching document
-
         if (mobileLinks.style.display === "block" || mobileLinks.style.display === "") {
             mobileLinks.style.display = "none";
         } else {
             mobileLinks.style.display = "block";
         }
+
     });
 
     // Add a click event listener to the document to hide mobile links
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Toggle the arrow icon
             const arrow = faq.querySelector(".arrow");
-            arrow.textContent = faq.classList.contains("expanded") ? "▼" : "▶️";
+            arrow.textContent = faq.classList.contains("expanded") ? "▼" : "▶";
 
             // Toggle the answer visibility
             const answer = faq.querySelector(".answer");
